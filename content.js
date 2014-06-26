01c41252727c14732dbbcbe4960b620c91f02f7e
Dustan Kasten <dustan.kasten@gmail.com>
2014-06-26 09:59:57 -0400

    oops sorry junior have some content

---
+++ content.js
@@ -0,0 +1,41 @@
+module.exports = {
+  kids: [
+    {
+      name: 'Guppies',
+      age: 'Nursery',
+      content: [
+        'This is some body copy.',
+        'This is some body copy.',
+        'This is some body copy.',
+      ]
+    },
+    {
+      name: 'Minnows',
+      age: 'Preschool',
+      content: [
+        'This is some body copy.',
+        'This is some body copy.',
+        'This is some body copy.',
+      ]
+    },
+    {
+      name: 'Lilly Pad',
+      age: '4 years - K',
+      content: [
+        'This is some body copy.',
+        'This is some body copy.',
+        'This is some body copy.',
+      ]
+    },
+    {
+      name: 'Splash',
+      age: '1st - 6th',
+      content: [
+        'This is some body copy.',
+        'This is some body copy.',
+        'This is some body copy.',
+      ]
+    }
+  ]
+}
+
