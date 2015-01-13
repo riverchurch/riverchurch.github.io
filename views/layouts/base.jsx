97d487a66e4f8b3cbca33230049cd1c033c29718
Dustan Kasten <dustan.kasten@gmail.com>
2015-01-12 23:26:53 -0500

    google analytics

--- views/layouts/base.jsx
+++ views/layouts/base.jsx
@@ -23,6 +23,7 @@ var Layout = React.createClass({
           </footer>
           <script dangerouslySetInnerHTML={{__html: 'var __rehydration = ' + JSON.stringify(this.props.rehydration) + ';'}}></script>
           <script src={`/public/js/${this.props.module}.js`}></script>
+          <script dangerouslySetInnerHTML={{__html: "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');ga('create', 'UA-32386434-1', 'auto');ga('send', 'pageview');"}}></script>
         </body>
       </html>
     );
